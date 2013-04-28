900ebf5011cb2f1adeefcedf469a1f504992921f
leandro <leandrogehlen@gmail.com>
2013-04-28 10:14:04 -0300

    Added combotree component

---
+++ widgets/EuiCombotree.php
@@ -0,0 +1,64 @@
+<?php 
+
+Yii::import('ext.yii-easyui.widgets.EuiContainer');
+
+class EuiCombotree extends EuiContainer {
+	
+	/**	 
+	 * @var string	a URL to retrieve remote data
+	 */
+	public $url;
+
+	/**	 
+	 * @var string	The http method to retrieve data
+	 */
+	public $method;
+
+	/**	 
+	 * @var boolean	Defines if to show animation effect when node expand or collapse
+	 */
+	public $animate;
+
+	/**	 
+	 * @var boolean	Defines if to show the checkbox before every node
+	 */
+	public $checkbox;
+
+	/**	 
+	 * @var boolean	Defines if to cascade check
+	 */
+	public $cascadeCheck;
+
+	/**	 
+	 * @var boolean	Defines if to show the checkbox only before leaf node
+	 */
+	public $onlyLeafCheck;
+	
+	/**
+	 * @var boolean	Defines if to show tree lines
+	 */
+	public $lines;
+
+	/**	 
+	 * @var boolean	Defines if to enable drag and drop
+	 */
+	public $dnd;
+
+	/**	 
+	 * @var array	The node data to be loaded
+	 */
+	public $data = array();
+	
+	
+	protected function getCssClass()
+	{
+		return 'easyui-combotree';
+	}
+		
+	public function run()
+	{
+		echo CHtml::tag('select', $this->toOptions());		
+	}
+}
+
+?>
