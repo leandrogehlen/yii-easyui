9654cb0a1c2b57df36eecdffed6c1a1b3d99fcb5
leandrogehlen <leandrogehlen@gmail.com>
2013-09-27 17:36:41 -0300

    Added treegrid component

---
+++ widgets/EuiTreegrid.php
@@ -0,0 +1,28 @@
+<?php
+
+Yii::import('ext.yii-easyui.widgets.EuiDataGrid');
+
+class EuiTreegrid extends EuiDataGrid 
+{	
+	
+	/**
+	 * @var string Defines the tree node field
+	 */
+	public $idField;
+	
+	/**
+	 * @var string	Defines the tree node field. required
+	 */
+	public $treeField;
+	
+	/**
+	 * @var boolean	Defines if to show animation effect when node expand or collapse
+	 */
+	public $animate;
+	
+		
+	protected function getCssClass()
+	{
+		return 'easyui-treegrid';
+	}
+}
