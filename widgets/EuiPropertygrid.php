5d096f73fe5f61cbb5ff1f73dc37d223fc7eead4
leandrogehlen <leandrogehlen@gmail.com>
2013-09-28 09:36:31 -0300

    Added "PropertyGrid" widget

---
+++ widgets/EuiPropertygrid.php
@@ -0,0 +1,30 @@
+<?php
+
+Yii::import('ext.yii-easyui.widgets.EuiDataGrid');
+
+class EuiPropertygrid extends EuiDataGrid 
+{	
+	
+	/**
+	 * @var boolean	Defines if to show property group.
+	 */
+	public $showGroup;
+	
+	/**
+	 * @var string	Defines the group field name.
+	 */
+	public $groupField;
+	
+	/**
+	 * @var string Defines how to format the group value. This function takes following parameters:
+	 * group: the group field value.
+     * rows: the rows belong to its group.
+	 */
+	public $groupFormatter;
+	
+		
+	protected function getCssClass()
+	{
+		return 'easyui-propertygrid';
+	}
+}
